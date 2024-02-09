8f4a3458aeec95847bd450dc7619d934bf3e91b5
KseniyaU <140713902+KseniyaU@users.noreply.github.com>
2024-02-10 00:05:14 +0200

    3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,19 +6,19 @@ import css from '../App/App.module.css'
 import { Navbar } from '../Navbar/Navbar'
 import HomePage from '../../pages/HomePage/HomePage.jsx'
 import MoviesPage from '../../pages/MoviesPage/MoviesPage.jsx'
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage.jsx';
 
-
+//45:27
 
 
 export function App() {
-  
   return (
     <div>
       <Navbar/>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/movies' element={<MoviesPage />} />
-        <Route path='*' element={ <div>Error</div>} />
+        <Route path='*' element={ <NotFoundPage/>} />
       </Routes>
     </div>
   )
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,37 @@
 import css from '../HomePage/HomePage.module.css'
+import { useEffect, useState } from 'react'
+//npm install axios
+import axios from 'axios'
 
 export default function HomePage() {
+    const keyAPI = '0511023512c6ae2508581d7ea4102544';
+    const [movies, setMovies] = useState([]);
+    const controller = new AbortController();
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${keyAPI}&language=en-US`,
+                    {
+                        signal: controller.signal,
+                    }
+                )
+                console.log(response.data);
+                setMovies(prevMovies =>[...prevMovies, ...response.data])
+            } catch (error) {
+                if (error.code !== 'ERR_CANCELED') {
+                    setError(true)
+                    console.log(error);
+                }
+                
+            }
+        }
+        fetchData();
+        return () => {
+            controller.abort();
+        };
+    }, [])
     return <div>Це наш HomePage</div>;
     
 }
