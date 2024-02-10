a6a42ffd59dc0b29177ce721a30412e30ce70ada
KseniyaU <140713902+KseniyaU@users.noreply.github.com>
2024-02-10 13:30:56 +0200

    4

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,16 +2,21 @@ import css from '../HomePage/HomePage.module.css'
 import { useEffect, useState } from 'react'
 //npm install axios
 import axios from 'axios'
+import { ErrorMessage } from '../../components/ ErrorMessage/ ErrorMessage';
+import { Loader } from '../../components/Loader/Loader';
 
 export default function HomePage() {
     const keyAPI = '0511023512c6ae2508581d7ea4102544';
     const [movies, setMovies] = useState([]);
     const controller = new AbortController();
     const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         async function fetchData() {
             try {
+                setLoading(true)
+                // setError(false)??????
                 const response = await axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${keyAPI}&language=en-US`,
                     {
                         signal: controller.signal,
@@ -25,6 +30,8 @@ export default function HomePage() {
                     console.log(error);
                 }
                 
+            } finally {
+                setLoading(false)
             }
         }
         fetchData();
@@ -32,6 +39,9 @@ export default function HomePage() {
             controller.abort();
         };
     }, [])
-    return <div>Це наш HomePage</div>;
+    return <div>Це наш HomePage
+        {error && <ErrorMessage />}
+        {loading && <Loader/>}
+    </div>;
     
 }
